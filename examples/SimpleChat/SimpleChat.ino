b3fd3bb2eda1078d2fbaf3553f3cedd85f60875f
Erik Kallen <info@erikkallen.nl>
2011-12-29 13:38:39 +0100

    Updated examples

---
+++ examples/SimpleChat/SimpleChat.ino
@@ -0,0 +1,36 @@
+#include <SPI.h>
+#include <RF12B.h>
+#include <rf_packet.h>
+
+void setup() {	
+	Serial.begin(57600);
+	RF12.begin();
+
+	Serial.println("Simple Chat started");
+}
+byte buf[50];
+int i = 0;
+void loop() {
+	if (Serial.available()>0) {
+		// Read from serial
+		char c = Serial.read();
+		buf[i++] = c;
+		if (c == '\n') {
+			RF12.sendPacket(buf, i);
+			// Clear rest of buffer for easy printing
+			memset(&buf[i-1],'\0',50-i);
+			buf[i+1] = '\0';
+			Serial.println("Sent:");
+			Serial.println((char *)buf);
+			i=0;
+		}
+	}
+	if (RF12.packetAvailable()) {
+		RFPacket p = RF12.recvPacket();
+		// Only print valid packets
+		if (p.valid()) {
+			Serial.println("Received:");
+			Serial.print(p);
+		}
+	}
+}
